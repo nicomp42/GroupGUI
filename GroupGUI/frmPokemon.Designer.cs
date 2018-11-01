11ade5958bbe0ec0d0db38165be47e252112584d
Bill Nicholson <nicholdw@ucmail.uc.edu>
2018-11-01 12:18:09 -0400

    Added things to form

--- GroupGUI/frmPokemon.Designer.cs
+++ GroupGUI/frmPokemon.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GroupGUI
 {
-    partial class Form1
+    partial class frmPokemon
     {
         /// <summary>
         /// Required designer variable.
@@ -37,17 +37,17 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Go";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // frmPokemon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "frmPokemon";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
--- GroupGUI/frmPokemon.cs
+++ GroupGUI/frmPokemon.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GroupGUI
 {
-    public partial class Form1 : Form
+    public partial class frmPokemon : Form
     {
-        public Form1()
+        public frmPokemon()
         {
             InitializeComponent();
         }
