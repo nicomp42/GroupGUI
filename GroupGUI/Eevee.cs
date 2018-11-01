650dcdd8f78227736f0140bf99007713cfcd50f8
Tiffany Litteral <littertm@mail.uc.edu>
2018-11-01 16:01:15 -0400

    Tiffany's Pokemon: Eevee

---
+++ GroupGUI/Eevee.cs
@@ -0,0 +1,43 @@
+﻿using PocketMonsters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGUI
+{
+    class Eevee : Pokemon
+    {
+        private string mName;
+        public Eevee (String Name) 
+        {
+            this.Name = Name;
+        }
+        public Eevee()
+        {
+
+        }
+        public string Name
+        {
+            get
+            {
+                return mName;
+            }
+            set
+            {
+                mName = value;
+            }
+        }
+
+        public override string Color()
+        {
+            return "Eevee's color is Brown ";
+        }
+
+        public override int Strength()
+        {
+            return 100;
+        }
+    }
+}
