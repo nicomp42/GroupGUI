ccded7cd6ee84b14e9a919c349bb526790c08467
Alexander Hargett <hargetaj@mail.uc.edu>
2018-11-01 16:06:40 -0400

    Updated Bimpley class so we know who created it

--- GroupGUI/Bimpley.cs
+++ GroupGUI/Bimpley.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/******************************************************************
+Alex Hargett
+InClass Pokemon Example
+Contemprary Programing (001)
+2188-1_34IT3045C001 (18FS-Full)
+Description: Create a new class for a pokemon that inherits from the pokemon class
+Date: 11/01/2018
+*******************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
