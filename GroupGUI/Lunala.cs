c7634e580d6f99adba64850c8a997e79b8b6310b
richbj <richbj@MCD210-PC-15>
2018-11-01 16:15:40 -0400

    Added comments to the lunala class

--- GroupGUI/Lunala.cs
+++ GroupGUI/Lunala.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/***************************************************
+ * Name: Bruce Rich
+ * 
+ * CLass number and section: IT3045C 001
+ * 
+ * Assignment: In class GroupGUI
+ * 
+ * Due Date: 11/1/2018
+ * 
+ * *Description:  This class implements the Strength and Color methods from the Pokemon class.
+ * 
+ * Citation: Paul Deitel "Visual C# How to Program"
+ * 
+ * Anything else:
+ **************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
