6a512374c314962bf8fd4f0e34013f967b6a58db
Bill Nicholson <nicholdw@ucmail.uc.edu>
2018-11-01 12:13:28 -0400

    Added virtual Pokemon class

---
+++ GroupGUI/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketMonsters
+{
+    abstract class Pokemon
+    {
+        private String mName;
+        public String name
+        {
+            get { return mName; }
+            set
+            {
+                if (value.Trim().Length == 0)
+                {
+                    throw new Exception("PocketMonsters.Pokemon.set Name: length cannot be zero");
+                }
+            }
+        }
+        public abstract int Strength();
+        public abstract String Color();
+    }
+}
