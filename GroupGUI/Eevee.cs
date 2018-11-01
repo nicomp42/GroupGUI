e00a412d1731e9c95b08bd5179a577cdfd996ca1
Robert Griffin <robacdcert@gmail.com>
2018-11-01 16:20:39 -0400

    Added Azzip class for the Azzip Pokemon.

--- GroupGUI/Eevee.cs
+++ GroupGUI/Eevee.cs
@@ -14,7 +14,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GroupGUI
+namespace PocketMonsters
 {
     class Eevee : Pokemon
     {/// <summary>
--- GroupGUI/frmPokemon.cs
+++ GroupGUI/frmPokemon.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GroupGUI
+namespace PocketMonsters
 {
     public partial class frmPokemon : Form
     {
