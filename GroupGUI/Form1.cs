687e7560692e500853f49152e4ce6831a56545b0
Bill Nicholson <nicholdw@ucmail.uc.edu>
2018-11-01 12:07:09 -0400

    Second Commit

--- GroupGUI/Form1.cs
+++ GroupGUI/Form1.cs
@@ -16,5 +16,14 @@ namespace GroupGUI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessPokemon();
+        }
+        private void ProcessPokemon()
+        {
+
+        }
     }
 }
