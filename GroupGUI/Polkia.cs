6dca0fa3224b859fbf93f484cb912e640fad5727
bromernn <bromernn@MCD210-PC-14>
2018-11-01 16:01:53 -0400

    Nathan's Pokemon

---
+++ GroupGUI/Polkia.cs
@@ -0,0 +1,26 @@
+﻿using PocketMonsters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGUI
+{
+    class Polkia : Pokemon
+    {
+        public Polkia(string name) : base() { }
+
+        public override string Color()
+        {
+            string color = "This is a shiny Polkia. It's colors are Pink and Purple.";
+            return color;
+        }
+
+        public override int Strength()
+        {
+            int strength = 99;
+            return strength;
+        }
+    }
+}
